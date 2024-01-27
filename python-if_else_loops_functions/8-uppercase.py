3702e33f2e16cf3b35d4924d415376fe312f23af
Bigguvix <nahufeola@hotmail.com>
2024-01-27 08:21:02 -0800

    #8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[I]) <= 122:
+            num = 32
+        else:
+            num = 0
+        print("{:c}".format(ord([stri]) - num), end="")
+        print()
