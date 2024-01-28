0b86bb2bed635f0f5951a90efdae36d0193267cc
Bigguvix <nahufeola@hotmail.com>
2024-01-28 06:48:31 -0800

    ·fixed 3add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) <= 1:
-        print("{}".format(len(argv)-1))
-         else:
-             sum = 0
-             for i in range(1, len(argv)):
-                 sum += int(argv[i])
-            print("{}".format(sum))
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
