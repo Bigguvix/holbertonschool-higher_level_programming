973fa6cfd963820d3fb3a14e24223030a5de1f87
Bigguvix <nahufeola@hotmail.com>
2024-02-04 07:27:54 -0800

    #fixed 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-
-    try:
-        for i in my_list:
-            if idx < x:
-                print("{}".format(my_list[idx]), end="")
-        
-        print()
-    
-    except TypeError:
-        pass
-    finally:
-        return idx
+    printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+             
+        except:
+            continue
+    print()
+    return printed
