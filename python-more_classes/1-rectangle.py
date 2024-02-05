7b1ae86086f1210e69eceae3d403647921dabc82
Bigguvix <nahufeola@hotmail.com>
2024-02-05 10:26:36 -0800

    #this is it, god help me

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,9 +8,9 @@ class Rectangle:
     """represents a rectangle"""
 
 
-def __init__(self, width=0, height=0):
-    self.height = height
-    self.width = width
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
