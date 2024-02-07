93402c3bcec80e75edfe8fbc50661e1abf8020fc
Bigguvix <nahufeola@hotmail.com>
2024-02-07 09:45:53 -0800

    #come on

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -83,13 +83,14 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """
-        returns a new Rectangle instance with width == height == size
+        Class method that defines a square
 
         Attributes:
-            cls: points to the class
-            size(int): size of square
+            cls : The parameter that points to the class
+            size (int): The size of the square
 
-        returns: width = height = size
+        Returns:
+            width = height = size
 
         """
-        return (cls(size, size))
+        return cls(size, size)
