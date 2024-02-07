2de555070072de79c9505f8c0e7965d45d267b52
Bigguvix <nahufeola@hotmail.com>
2024-02-07 09:10:12 -0800

    #fixed pycodestyle 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
     def __del__(self):
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
