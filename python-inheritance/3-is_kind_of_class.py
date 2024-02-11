d7eb49fa50e7f895e932efed33f61d185ce6b4ab
Bigguvix <nahufeola@hotmail.com>
2024-02-11 08:09:38 -0800

    #3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""find if the object is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """finds if obj is an instance of a_class or a class"""
+    return isinstance(obj, a_class)
