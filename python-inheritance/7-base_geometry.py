ebecaf4d0dd1afdafeefb39af2b245f63c62ce5e
Bigguvix <nahufeola@hotmail.com>
2024-02-11 08:29:05 -0800

    #7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 7-base_geometry.py
+"""Creates a class."""
+
+
+class BaseGeometry:
+    """Class with public instance method."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
