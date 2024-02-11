e6dbfea4fb9a6e18bbf080ba18a745a6bebe4207
Bigguvix <nahufeola@hotmail.com>
2024-02-11 09:26:20 -0800

    #fixed typo

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@
 def write_file(filename="", text=""):
     """Function that writes to a text file"""
 
-    with opem(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
