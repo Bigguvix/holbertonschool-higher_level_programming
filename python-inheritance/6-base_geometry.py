cc27da696963eef148b980b05da89218be3174d2
Bigguvix <nahufeola@hotmail.com>
2024-02-11 08:22:31 -0800

    #6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""Creates a class."""
+
+
+class BaseGeometry:
+    """Class with public instance method."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
