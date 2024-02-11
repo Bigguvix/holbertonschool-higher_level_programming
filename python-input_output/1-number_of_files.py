3da230ea6b228a39fb8de992d37234764f6e668f
Bigguvix <nahufeola@hotmail.com>
2024-02-11 09:12:55 -0800

    #1

---
+++ python-input_output/1-number_of_files.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#  1-number_of_lines.py
+"""Count number of lines"""
+
+
+def number_of_lines(filename=""):
+    """Counts lines in filename"""
+    
+    count = 0
+
+    with open(filename) as f:
+        text = f.readlines()
+        for line in text:
+            count += 1
+    return count
