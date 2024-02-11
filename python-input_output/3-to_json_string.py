5dd0d4565341a1b63e9c687ba0fa6a120c7d4c84
Bigguvix <nahufeola@hotmail.com>
2024-02-11 09:39:50 -0800

    #3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 3-to_json_String.py
+"""return json representations"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns json represemtation"""
+    return json.dumps(my_obj)
