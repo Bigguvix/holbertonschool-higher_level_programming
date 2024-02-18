3d881493054b08f5e39ff5fb35914a6ee000a991
Bigguvix <nahufeola@hotmail.com>
2024-02-18 08:36:28 -0800

    #created square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# square.py
+"""Contains a class square that inherits from
+class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes instances """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Special method"""
+
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
